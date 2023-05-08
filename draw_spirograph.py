64802ce879c4a0be0c7a638818fea81d2a654652
Reshma R <reshmarmk33@gmail.com>
2023-05-08 23:58:34 +0530

    initial commit for turtle

---
+++ draw_spirograph.py
@@ -0,0 +1,16 @@
+"""
+    TODO: Add description for this file
+"""
+from turtle import Turtle, Screen
+
+# Initialize turtle object to perform actions.
+turtle = Turtle()
+# Initialize the screen object to control the screen.
+screen = Screen()
+
+# Create Spirograph using Turtle
+turtle.speed("fastest")
+
+for _ in range(int(360/10)):
+    turtle.circle(100)
+    turtle.setheading(_*10)
